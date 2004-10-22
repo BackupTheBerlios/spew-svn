fad7c76336e7bfc3b63c486382fc0488518cbe46
apers <apers@766da9e2-65e6-0310-a730-b54d5056fad5>
2004-10-22 20:06:38 +0000

    Moved email java script to an include file.

---
+++ web/include/andrew-spew-email.js
@@ -0,0 +1,57 @@
+// <!-- 451235096
+// This script is (C) Copyright 2004 Jim Tucek
+// Leave these comments alone!  For more info, visit
+// www.jracademy.com/~jtucek/email/ 
+
+function cling(hug,steel,agreement) {
+hug += ' ';
+var cleverness = hug.length;
+var kiss = 0;
+var biologist = '';
+for(var forest = 0; forest < cleverness; forest++) {
+kiss = 0;
+while(hug.charCodeAt(forest) != 32) {
+kiss = kiss * 10;
+kiss = kiss + hug.charCodeAt(forest)-48;
+forest++;
+}
+biologist += String.fromCharCode(saw(kiss,steel,agreement));
+}
+parent.location = 'm'+'a'+'i'+'l'+'t'+'o'+':'+biologist;
+}
+
+function owe(chloride,confidence,contact) {
+chloride += ' ';
+var corporation = chloride.length;
+var cost = 0;
+for(var credibility = 0; credibility < corporation; credibility++) {
+cost = 0;
+while(chloride.charCodeAt(credibility) != 32) {
+cost = cost * 10;
+cost = cost + chloride.charCodeAt(credibility)-48;
+credibility++;
+}
+//document.write('&');
+//document.write('#');
+//document.write(saw(cost,confidence,contact));
+document.write(String.fromCharCode(saw(cost,confidence,contact)));
+}
+}
+
+function saw(dam,energy,favor) {
+if (favor % 2 == 0) {
+genetics = 1;
+for(var hand = 1; hand <= favor/2; hand++) {
+law = (dam*dam) % energy;
+genetics = (law*genetics) % energy;
+}
+} else {
+genetics = dam;
+for(var monkey = 1; monkey <= favor/2; monkey++) {
+law = (dam*dam) % energy;
+genetics = (law*genetics) % energy;
+}
+}
+return genetics;
+}
+// -->
